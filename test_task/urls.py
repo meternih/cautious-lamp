badae1a91a1f66afd6d5c36c486d78f1901c98b0
meternih <meternih@gmail.com>
2021-04-30 01:48:54 +0300

    init repository

--- test_task/urls.py
+++ test_task/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'', views.user_list),
+    url(r'', views.user_detail),
 ]
